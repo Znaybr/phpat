54d7e9d3a9b7437d52c5e2d06ede8f37ae5693a0
tverjus <thibaut.verjus@gmail.com>
2015-12-17 12:54:32 -0500

    Premier commits . Base du projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "_defines.php";
+require_once "view_parts/_page_base.php"; // référence au fichier de référence page_base = HEAD en HTML
+?>
+
+<div id="main"></div>
+
+<?php
+require_once "view_parts/_footer.php"; // référence au fichier de référence page_base = HEAD en HTML
+?>
+
+
---
+++ view_parts/_page_base.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= SITE_NAME ?></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Gruppo' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<?php
+    require_once "_header.php";
+?>
