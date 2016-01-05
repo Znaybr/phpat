cd70314d5d8c970c50a86e496691dccbf0db03ce
tverjus <thibaut.verjus@gmail.com>
2016-01-05 16:49:53 -0500

    validation formulaire amélioration :
    Messages d'erreurs individualisés par champs

--- view_parts/_inscription.php
+++ view_parts/_inscription.php
@@ -2,16 +2,16 @@
 
 $in_post=array_key_exists("register", $_POST); // Savoir si le formulaire est en soumission/reception
 
-$message_erreur="";
 
 $prenom_ok = false;
+$warning_prenom = ""; //message de feedback en cas de champ erronné
 $prenom_message = ""; //message de feedback en cas de champ erronné, affiché si non vide
 if (array_key_exists("prenom", $_POST)) {
     $prenom = filter_input(INPUT_POST, "prenom", FILTER_SANITIZE_STRING );
     $prenom_ok = (1 === preg_match("/^[A-Za-z0-9]{2,}$/", $prenom));  // 1 siginifie que la condition est vraie et vérifiée
     if(!$prenom_ok){ // si prenom est non valide
-        $prenom_message=" *";
-        $message_erreur=" Merci de corriger les champs comportants un *.";
+        $warning_prenom=" *";
+        $prenom_message="Votre prénom doit comporter au moins deux lettres";
     }
 /*    var_dump($prenom);
     var_dump($prenom_ok);
@@ -19,27 +19,31 @@ if (array_key_exists("prenom", $_POST)) {
 }
 
 $nom_ok = false;
+$warning_nom = ""; //message de feedback en cas de champ erronné
 $nom_message = ""; //message de feedback en cas de champ erronné, affiché si non vide
 if (array_key_exists("nom", $_POST)) {
     $nom = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_STRING );
     $nom_ok = (1 === preg_match("/^[A-Za-z0-9]{2,}$/", $nom));  // 1 siginifie que la condition est vraie et vérifiée
     if(!$nom_ok){ // si nom est non valide
-        $nom_message=" *";
-        $message_erreur=" Merci de corriger les champs comportants un *.";
+        $warning_nom=" *";
+        $nom_message="Votre nom doit comporter au moins deux lettres";
+
     }
     /*    var_dump($nom);
         var_dump($nom_ok);*/
 }
 
 $courriel_ok = false;
+$warning_courriel = ""; //message de feedback en cas de champ erronné
 $courriel_message = ""; //message de feedback en cas de champ erronné, affiché si non vide
 if (array_key_exists("courriel", $_POST)) {
     $courriel = filter_input(INPUT_POST, "courriel", FILTER_SANITIZE_EMAIL );
     $courriel = filter_var($courriel, FILTER_VALIDATE_EMAIL);
     $courriel_ok = (false !== $courriel);
     if(!$courriel_ok) { // si nom est non valide
-        $courriel_message = " *";
-        $message_erreur=" Merci de corriger les champs comportants un *.";
+        $warning_courriel=" *";
+        $courriel_message="Ce champs doit comporter une adresse mail valide";
+
     }
     // PAS DE PREGMATCH puisque les filtres font déjà la selection
 /*    var_dump($courriel);
@@ -48,12 +52,13 @@ if (array_key_exists("courriel", $_POST)) {
 
 $pseudo_ok = false;
 $pseudo_message="";
+$warning_pseudo = ""; //message de feedback en cas de champ erronné
 if (array_key_exists("pseudo", $_POST)) {
     $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_STRING );
     $pseudo_ok = (1 === preg_match("/^[A-Za-z0-9]{4,}$/", $pseudo));  // 1 siginifie que la condition est vraie et vérifiée
     if(!$pseudo_ok){ // si nom est non valide
-        $pseudo_message=" *";
-        $message_erreur=" Merci de corriger les champs comportants un *.";
+        $warning_pseudo=" *";
+        $pseudo_message="Votre pseudo doit comporter au moins quatres lettres";
     }
     /*    var_dump($pseudo);
         var_dump($pseudo_ok);*/
@@ -61,12 +66,13 @@ if (array_key_exists("pseudo", $_POST)) {
 
 $mdp_ok = false;
 $mdp_message="";
+$warning_mdp = ""; //message de feedback en cas de champ erronné
 if (array_key_exists("mdp", $_POST)) {
     $mdp = filter_input(INPUT_POST, "mdp", FILTER_SANITIZE_STRING );
     $mdp_ok = (1 === preg_match("/^[A-Za-z0-9%&!*?]{8,}$/", $mdp));  // 1 siginifie que la condition est vraie et vérifiée
     if(!$mdp_ok){ // si nom est non valide
-        $mdp_message=" *";
-        $message_erreur=" Merci de corriger les champs comportants un *.";
+        $warning_mdp=" *";
+        $mdp_message="Votre mot de passe doit comporter au moins 8 caractères";
     }
     /*    var_dump($mdp);
         var_dump($mdp_ok);*/
@@ -78,36 +84,47 @@ if ($nom_ok && $prenom_ok && $courriel_ok && $pseudo_ok && $mdp_ok){
     //on enregistre les données et s'en va sur une autres page
 }
 
+
 ?>
 
 <form id="inscription" action="inscription.php" method="post">
     <ul>
-        <li><label for="prenom">Prénom <span><?php echo $prenom_message ?></span></label>
+        <li><label for="prenom">Prénom <span><?php echo $warning_prenom ?></span></label>
             <input type="text" id="prenom" name="prenom"
                    class="<?php echo $in_post && ! $prenom_ok ? 'erreur' : ''; ?>"
-                   value="<?php echo array_key_exists('prenom', $_POST) ? $_POST['prenom']: ''?>"
+                   value="<?php echo array_key_exists('prenom', $_POST) ? $_POST['prenom']: ''?>"/>
         </li>
-        <li><label for="nom">Nom <span><?php echo $nom_message ?></span></label>
+        <span><?php echo $prenom_message ?></span>
+
+        <li><label for="nom">Nom <span><?php echo $warning_nom ?></span></label>
             <input type="text" id="nom" name="nom"
                    class="<?php echo $in_post && ! $nom_ok ? 'erreur' : ''; ?>"
-                   value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom']: ''?>"</li>
+                   value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom']: ''?>"/>
+        </li>
+        <span><?php echo $nom_message ?></span>
 
-        <li><label for="courriel">Courriel <span><?php echo $courriel_message ?></span></label>
+        <li><label for="courriel">Courriel <span><?php echo $warning_courriel ?></span></label>
             <input type="text" id="courriel" name="courriel"
                    class="<?php echo $in_post && ! $courriel_ok ? 'erreur' : ''; ?>"
-                   value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel']: ''?>"</li>
+                   value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel']: ''?>"/>
+        </li>
+        <span><?php echo $courriel_message ?></span>
 
-        <li><label for="pseudo">Pseudo <span><?php echo $pseudo_message ?></span></label>
+        <li><label for="pseudo">Pseudo <span><?php echo $warning_pseudo ?></span></label>
             <input type="text" id="pseudo" name="pseudo"
                    class="<?php echo $in_post && ! $pseudo_ok ? 'erreur' : ''; ?>"
-                   value="<?php echo array_key_exists('pseudo', $_POST) ? $_POST['pseudo']: ''?>"</li>
+                   value="<?php echo array_key_exists('pseudo', $_POST) ? $_POST['pseudo']: ''?>"/>
+        </li>
+        <span><?php echo $pseudo_message ?></span>
 
-        <li><label for="mdp">Mot de passe<span><?php echo $mdp_message ?></span></label>
+        <li><label for="mdp">Mot de passe<span><?php echo $warning_mdp ?></span></label>
             <input type="password" id="mdp" name="mdp"
                    class="<?php echo $in_post && ! $mdp_ok ? 'erreur' : ''; ?>"
-                   value="<?php echo array_key_exists('mdp', $_POST) ? $_POST['mdp']: ''?>"></li>
+                   value="<?php echo array_key_exists('mdp', $_POST) ? $_POST['mdp']: ''?>"/>
+        </li>
+        <span><?php echo $mdp_message ?></span>
 
         <li><input type="submit" value="Valider" id="register" name="register"></li>
     </ul>
-    <span> <?php echo $message_erreur ?> </span>
+    <?php if($in_post){ echo "<p>Merci de corriger les champs comportants un *.</p>"; } ?>
 </form>
